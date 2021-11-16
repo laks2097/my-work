f88c81d16c2c2f1e9137d8d7390f435d91bea0b2
Lakshay Bhalla - l0b0a68 <Lakshay.Bhalla@walmart.com>
2021-11-16 10:20:29 +0530

    Commiting initial Changes

---
+++ react-hello-world/app.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+
+const placeholder = document.getElementById('placeholder');
+
+
+const Home = React.createElement('a',{href:"/",style:{color:"red",textDecoration:"none",fontWeight:"bold",fontSize:"2rem"},className:"link-ref"},"Home");
+const about = React.createElement('a',{href:'/about',style:{color:"green",fontWeight:"bold",textDecoration:"none",paddingRight:'5px',fontSize:"2rem"}},"About");
+// const br = React.createElement('br',null,null);
+const h1Tag = React.createElement("h1",null,"Welcome to React");
+const headerStuff = React.createElement('div',
+{
+    style:{
+        display:"flex",
+        flexDirection:"row",
+        alignContent:"center",
+        justifyContent:"space-between",
+        flexWrap:"nowrap",
+        alignItems:"center",
+        backgroundColor: '#a3ffff',
+        padding:'5px'
+    }
+}, h1Tag,Home,about);
+
+//placeholder.innerHTML = app;
+const pTag = React.createElement("p",{style:{marginLeft:"10px"}},"This is my first React project.");
+const h2Tag = React.createElement("h2",{style:{marginLeft:"10px"}},"Home Page");
+const body = React.createElement("div",{className:"main-body"},headerStuff,h2Tag,pTag);
+ReactDOM.render(body, placeholder);
+// ReactDOM.render(app, placeholder)
